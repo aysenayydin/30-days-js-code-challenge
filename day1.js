76c2cd4ba5fbc22a10aa3d0f6abb30a086f1c23f
Aysen Aydin <aysen904@gmail.com>
2024-08-18 13:53:33 +0300

    create day1 challenge

---
+++ day1.js
@@ -0,0 +1,19 @@
+
+let i2, d2, s2;
+i2=parseInt(readLine());
+d2=parseFloat(readLine());
+s2=readLine();
+console.log(i+i2)
+console.log(parseFloat(d+d2).toFixed(1))
+console.log(s+s2)
+
+    // Declare second integer, double, and String variables.
+
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
